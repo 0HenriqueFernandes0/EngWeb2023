288632b5993ecc6ff6b76b5813b2331d8da4b870
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-03-20 14:45:08 +0000

    avanço a mostrar as tareefas da basee de dados

--- TPC5/routes/index.js
+++ TPC5/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+Tasks = require('../controllers/tasks')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = new Date().toISOString().substring(0, 16)
+  Tasks.list()
+  .then(tasks => {
+    res.render('index', { t: tasks, d: data });
+  })
+  .catch(erro => {
+    res.render('error', {error: erro, message: "Erro na obtenção da lista de tasks"})
+  })
+  
 });
 
 module.exports = router;
