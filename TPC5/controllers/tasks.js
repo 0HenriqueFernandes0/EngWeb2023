0f30d73626b0725f3bf3731a4bf65e88c405d425
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-03-20 21:20:04 +0000

    delete e done feitos

--- TPC5/controllers/tasks.js
+++ TPC5/controllers/tasks.js
@@ -10,3 +10,45 @@ module.exports.list = () => {
                 return erro
             })
 }
+
+module.exports.Done = (id) => {
+    return axios.get('http://localhost:3000/tasks/'+id)
+        .then(resposta => {
+            task= resposta.data
+            task.Done = 1
+            return axios.put('http://localhost:3000/tasks/'+id,task)
+                .then(resposta => {
+                    return axios.get('http://localhost:3000/tasks')
+                        .then(resposta => {
+                            return resposta.data
+                        })
+                        .catch(erro => {
+                            return erro
+                        })
+                })
+                .catch(erro => {
+                    return erro
+                })
+        })
+        .catch(erro => {
+            return erro
+        })
+
+}
+
+module.exports.Delete = (id) => {
+    return axios.delete('http://localhost:3000/tasks/'+id)
+        .then(resposta=>{
+            return axios.get('http://localhost:3000/tasks')
+            .then(resposta => {
+                return resposta.data
+            })
+            .catch(erro => {
+                return erro
+            })
+        })
+        .catch(erro => {
+            return erro
+        })
+
+}
--- TPC5/routes/index.js
+++ TPC5/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 Tasks = require('../controllers/tasks')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('(/|/tasks)', function(req, res, next) {
   var data = new Date().toISOString().substring(0, 16)
   Tasks.list()
   .then(tasks => {
@@ -12,6 +12,42 @@ router.get('/', function(req, res, next) {
   .catch(erro => {
     res.render('error', {error: erro, message: "Erro na obtenção da lista de tasks"})
   })
+  
+});
+
+router.get('/tasks/Edit/:idTasks', function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16)
+  Tasks.list()
+    .then(tasks => {
+      res.render('index', { t: tasks, d: data });
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção da lista de tasks"})
+    })
+  
+});
+
+router.get('/tasks/Done/:idTasks', function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16)
+  Tasks.Done(req.params.idTasks)
+    .then(tasks => {
+      res.render('index', { t: tasks, d: data });
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção da task"})
+    })
+  
+});
+
+router.get('/tasks/Delete/:idTasks', function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16)
+  Tasks.Delete(req.params.idTasks)
+    .then(tasks => {
+      res.render('index', { t: tasks, d: data });
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção da task"})
+    })
   
 });
 
